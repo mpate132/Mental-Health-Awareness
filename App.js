affff3f4b4c9303b1b7de5d8b5a3a110da9d2f7f
MahirPatel3130 <patelmahir042@gmail.com>
2023-01-14 12:20:17 -0800

    Deleted Backend

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import HomeScreen from "./screens/HomeScreen";
 import { NavigationContainer } from "@react-navigation/native";
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,13 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, Button } from "react-native";
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
   return (
     <SafeAreaView>
       <Text>HomeScreen</Text>
+      <Button title="Chat-Screen" onPress={() => navigation.navigate("Chat")} />
     </SafeAreaView>
   );
 };
